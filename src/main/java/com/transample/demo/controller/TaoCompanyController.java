7a3d415f1b4e3dbb0a704ab17a6bc7ca75922dd8
maguorong <617044423@qq.com>
2020-10-14 13:58:22 +0800

    更新物流公司taoCompany接口

--- src/main/java/com/transample/demo/controller/TaoCompanyController.java
+++ src/main/java/com/transample/demo/controller/TaoCompanyController.java
@@ -1,15 +1,15 @@
 package com.transample.demo.controller;
 
 import java.util.List;
+
+import com.transample.demo.common.ResultCode;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import com.transample.demo.domain.TaoCompany;
 import com.transample.demo.service.ITaoCompanyService;
 import com.transample.demo.common.ResponseResult;
@@ -29,15 +29,16 @@ public class TaoCompanyController
 	@Autowired
 	private ITaoCompanyService taoCompanyService;
 	
-	@GetMapping()
-	public String taoCompany()
-	{
-	    return prefix + "/taoCompany";
-	}
+//	@GetMapping()
+//	public String taoCompany()
+//	{
+//	    return prefix + "/taoCompany";
+//	}
 	
 	/**
 	 * 查询物流公司列表
 	 */
+	@ApiOperation(value = "查询物流公司列表 支持模糊查询")
 	@PostMapping("/list")
 	@ResponseBody
 	public List<TaoCompany> list(TaoCompany taoCompany)
@@ -47,21 +48,22 @@ public class TaoCompanyController
 	}
 	
 	
-	/**
-	 * 新增物流公司
-	 */
-	@GetMapping("/add")
-	public String add()
-	{
-	    return prefix + "/add";
-	}
+//	/**
+//	 * 新增物流公司
+//	 */
+//	@GetMapping("/add")
+//	public String add()
+//	{
+//	    return prefix + "/add";
+//	}
 	
 	/**
 	 * 新增保存物流公司
 	 */
+	@ApiOperation(value = "新增保存物流公司")
 	@PostMapping("/add")
 	@ResponseBody
-	public ResponseEntity addSave(TaoCompany taoCompany)
+	public ResponseEntity addSave(@RequestBody  TaoCompany taoCompany)
 	{		
 		return ResponseEntity.ok(ResponseResult.ok(taoCompanyService.insertTaoCompany(taoCompany)));
 	}
@@ -69,33 +71,40 @@ public class TaoCompanyController
 	/**
 	 * 修改物流公司
 	 */
+	@ApiOperation(value = "修改物流公司，返回物流公司的基本信息")
 	@GetMapping("/edit/{companyId}")
-	public String edit(@PathVariable("companyId") Integer companyId, ModelMap mmap)
+	public ResponseEntity edit(@PathVariable("companyId") @ApiParam(name = "物流公司的id") Integer companyId, ModelMap mmap)
 	{
 		TaoCompany taoCompany = taoCompanyService.selectTaoCompanyById(companyId);
+		if(taoCompany==null)
+		{
+			return ResponseEntity.ok(ResponseResult.fail(ResultCode.FILED_VALUE_INVALID));
+		}
 		mmap.put("taoCompany", taoCompany);
-	    return prefix + "/edit";
+		return ResponseEntity.ok(ResponseResult.ok(mmap));
+//	    return prefix + "/edit";
 	}
 	
 	/**
 	 * 修改保存物流公司
 	 */
+	@ApiOperation(value = "修改保存物流公司，返回状态")
 	@PostMapping("/edit")
 	@ResponseBody
-	public ResponseEntity editSave(TaoCompany taoCompany)
+	public ResponseEntity editSave(@RequestBody TaoCompany taoCompany)
 	{		
 		return ResponseEntity.ok(ResponseResult.ok(taoCompanyService.updateTaoCompany(taoCompany)));
 	}
 	
-	/**
-	 * 删除物流公司
-	 */
-
-	@PostMapping( "/remove")
-	@ResponseBody
-	public ResponseEntity remove(String ids)
-	{		
-		return ResponseEntity.ok(ResponseResult.ok(taoCompanyService.deleteTaoCompanyByIds(ids)));
-	}
+//	/**
+//	 * 删除物流公司
+//	 */
+//
+//	@PostMapping( "/remove")
+//	@ResponseBody
+//	public ResponseEntity remove(String ids)
+//	{
+//		return ResponseEntity.ok(ResponseResult.ok(taoCompanyService.deleteTaoCompanyByIds(ids)));
+//	}
 	
 }
--- src/main/java/com/transample/demo/domain/TaoCompany.java
+++ src/main/java/com/transample/demo/domain/TaoCompany.java
@@ -1,6 +1,9 @@
 package com.transample.demo.domain;
 
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+
 import java.util.Date;
 
 /**
@@ -11,20 +14,27 @@ import java.util.Date;
  */
 public class TaoCompany
 {
-	
+
 	/** 物流公司id */
+	@ApiModelProperty(value = "不传")
 	private Integer companyId;
+	@ApiModelProperty(value = "物流公司名称",required = true)
 	/** 物流公司名称 */
 	private String companyName;
 	/** 物流公司类型（常规/村县） */
+	@ApiModelProperty(value = "物流公司类型（常规/县村）",required = true)
 	private String companyType;
 	/** 	主管姓名 */
+	@ApiModelProperty(value = "主管姓名",required = true)
 	private String supervisorName;
 	/** 	主管电话 */
+	@ApiModelProperty(value = "主管电话",required = true)
 	private String supervisorTel;
 	/** 	创建时间 */
+	@ApiModelProperty(value = "不传")
 	private Date createTime;
 	/** 	更新时间 */
+	@ApiModelProperty(value = "不传")
 	private Date updateTime;
 
 	public void setCompanyId(Integer companyId) 
