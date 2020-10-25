4869392a4c226b2456abf0110f9e7712f9a93e1d
maguorong <617044423@qq.com>
2020-10-25 11:08:31 +0800

    解决登陆时的跨域问题

--- src/main/java/com/transample/demo/controller/UserController.java
+++ src/main/java/com/transample/demo/controller/UserController.java
@@ -12,11 +12,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 @Api(tags = "用户管理相关API")
